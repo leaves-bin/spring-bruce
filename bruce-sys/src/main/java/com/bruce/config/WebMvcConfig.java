cd2dbce757b339b2d76d957291f954d535709cbf
leaes-bin <zhangyongbin5@jd.com>
2021-10-14 09:32:04 +0800

    init

---
+++ bruce-sys/src/main/java/com/bruce/config/WebMvcConfig.java
@@ -0,0 +1,74 @@
+package com.bruce.config;
+
+import com.alibaba.fastjson.serializer.SerializeFilter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ValueFilter;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.bruce.common.BaseHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+/**
+ * WebMvcConfig
+ * 类描述：
+ *
+ * @author:zhangyongbin5
+ * @date:2021/9/29
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(new SerializerFeature[]{
+                SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.SortField,
+                SerializerFeature.MapSortField,
+                SerializerFeature.WriteBigDecimalAsPlain,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteDateUseDateFormat
+        });
+        ValueFilter filter = (object, name, value) -> {
+            if (value instanceof BigDecimal) {
+                return ((BigDecimal) value).stripTrailingZeros().toPlainString();
+            } else if (value instanceof LocalDateTime) {
+                return ((LocalDateTime) value).toEpochSecond(ZoneOffset.ofHours(8)) * 1000L;
+            } else {
+                return value instanceof LocalDate ? ((LocalDate) value).toEpochDay() * 86400000L - 28800000L : value;
+            }
+        };
+        fastJsonConfig.setSerializeFilters(new SerializeFilter[]{filter});
+        converters.add(new BaseHttpMessageConverter(fastJsonConfig));
+        super.configureMessageConverters(converters);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*").allowedMethods("*")
+                .allowedOrigins("*").allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+
+}
